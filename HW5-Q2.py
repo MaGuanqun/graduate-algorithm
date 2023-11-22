0b6bd973649f2b2a642b03c1f92371bf04a1a0dd
guanqun ma <guanqunma@outlook.com>
2023-11-22 14:52:16 -0700

    fixed Q2

--- HW5-Q2.py
+++ HW5-Q2.py
@@ -8,13 +8,14 @@ def simulate_random_walk(steps, runs):
         # Compute the cumulative sum to get the position at each step
         positions = np.cumsum(walk)
         # Count the number of times the walk crosses the origin
-        cross = np.sum((positions[:-1] * positions[1:]) < 0)
+        cross = np.sum(positions == 0)
+        # cross = np.sum((positions[:-1] * positions[1:]) < 0)
         crossings.append(cross)
     return np.mean(crossings)
 
 # Define the number of steps and runs
 steps_list = [4 * 10**4, 9 * 10**4, 16 * 10**4]
-runs = 1
+runs = 50
 
 # Run simulations and collect average crossings
 average_crossings = [simulate_random_walk(steps, runs) for steps in steps_list]
